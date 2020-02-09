5eba270a21719266833ff337623efcdedb478087
unknown <seambark@gmail.com>
2020-02-09 12:24:35 +0900

    수정

--- src/navbar.js
+++ src/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import styled from 'styled-compoments'
+import styled from 'styled-components'
 
 const Container = styled.div`
   font-size: 32px;
   font-weight: bold;
   padding: 15px 10px;
+  border: 1px solid #eee;
 `
 
 // function Navbar(props) {
